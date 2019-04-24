47cabeef6cde62edd03ad7ef58dea29a096a68c0
mmateu <45851518+mmateu@users.noreply.github.com>
2019-04-24 17:51:06 +0200

    Enable login-password authentication

--- presentation/src/main/java/com/example/BookTradingClub/presentation/security/SecurityConfig.java
+++ presentation/src/main/java/com/example/BookTradingClub/presentation/security/SecurityConfig.java
@@ -1,22 +1,27 @@
 package com.example.BookTradingClub.presentation.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/user/signup*").permitAll()
-                .antMatchers("/user-books*").permitAll()
+                .antMatchers("/user-books*").authenticated()
                 .antMatchers("/books").authenticated()
                 .and()
                 .httpBasic().and()
@@ -25,14 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER");
+        auth.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder);
     }
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+
 }
---
+++ presentation/src/main/java/com/example/BookTradingClub/presentation/security/UserPrincipal.java
@@ -0,0 +1,55 @@
+package com.example.BookTradingClub.presentation.security;
+
+import com.example.BookTradingClub.service.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+    private final User user;
+
+    public UserPrincipal(final User user) {
+        super();
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        final List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("USER"));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
