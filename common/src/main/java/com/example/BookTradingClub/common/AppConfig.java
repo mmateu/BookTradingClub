4707a53e710f4a383a4201590e9f3babab5d43af
mmateu <45851518+mmateu@users.noreply.github.com>
2019-04-24 17:48:08 +0200

    Move password encoder creation to common module

--- common/src/main/java/com/example/BookTradingClub/common/AppConfig.java
+++ common/src/main/java/com/example/BookTradingClub/common/AppConfig.java
@@ -3,6 +3,8 @@ package com.example.BookTradingClub.common;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,10 @@ public class AppConfig {
     @Bean
     ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
