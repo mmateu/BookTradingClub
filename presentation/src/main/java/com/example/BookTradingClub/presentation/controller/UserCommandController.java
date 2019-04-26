9f548f647538d70715fef7bb9094a4774539cc04
mmateu <45851518+mmateu@users.noreply.github.com>
2019-04-26 18:47:29 +0200

    Add GET endpoint for getting current user

--- presentation/src/main/java/com/example/BookTradingClub/presentation/controller/UserCommandController.java
+++ presentation/src/main/java/com/example/BookTradingClub/presentation/controller/UserCommandController.java
@@ -1,13 +1,17 @@
 package com.example.BookTradingClub.presentation.controller;
 
 import com.example.BookTradingClub.presentation.dto.RegistrationDto;
+import com.example.BookTradingClub.presentation.dto.UserDto;
 import com.example.BookTradingClub.service.UserCommandService;
+import com.example.BookTradingClub.service.domain.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping(path = "/user")
@@ -26,6 +30,16 @@ public class UserCommandController {
 
 
     }
+
+    @RequestMapping(value = "/current", method = RequestMethod.GET)
+    public UserDto getCurrentUser(Principal principal){
+        User user = userCommandService.findUserByName(principal.getName()) ;
+
+        return  mapper.map(user, UserDto.class);
+    }
+
+
+
 
 
 }
