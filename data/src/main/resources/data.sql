c76743905ce1a3433c68fa42745ec6f6a737bf22
mmateu <45851518+mmateu@users.noreply.github.com>
2019-04-29 12:08:37 +0200

    Add password field in datasource entities

--- data/src/main/resources/data.sql
+++ data/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO books(title,author) VALUES('title', 'author'),('Harry Potter i Komnata Tajemnic', 'J.K. Rowling'),
 ( 'To Kill a Mockingbird', 'Harper Lee'), ( 'Brave New World', 'Aldous Huxley');
-INSERT INTO users(id, name, first_name, last_name, city, country) VALUES(NULL, 'AegonTargaryen', 'John', NULL, NULL, NULL),
-(NULL, 'Jack', NULL,NULL, 'London',NULL);
+INSERT INTO users(id, name, password, first_name, last_name, city, country) VALUES(NULL, 'Jonas', '$2a$10$uFouhr392epnO2YkfQ8f1uUNZmmXq04riDpdDN2bC4zOq34FA60YO', 'John', NULL, NULL, NULL),
+(NULL, 'Jack','$2a$10$uFouhr392epnO2YkfQ8f1uUNZmmXq04riDpdDN2bC4zOq34FA60YO', NULL,NULL, 'London',NULL);
 INSERT INTO user_books(id, user_id, book_id, count) VALUES(NULL, 1,1,2), (NULL, 1,2,0), (NULL,2,2,1);
